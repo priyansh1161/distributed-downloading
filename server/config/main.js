b0765b393c1054770f9cc22ff700a4dfbdfcde24
priyansh gupta <priyanshgupta1161@gmail.com>
2017-03-02 19:08:20 +0530

    initail commit

---
+++ server/config/main.js
@@ -0,0 +1,7 @@
+module.exports.dev = {
+    db : 'mongodb://127.0.0.1:27017/pintrest',
+    apiURI : 'http://127.0.0.1:3000/api'
+};
+module.exports.production = {
+
+};
---
+++ server/models/db.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var configure = require('../config/main');
+
+var config = configure.dev;
+if(process.env.NODE_ENV=='production')
+    config = configure.production;
+var dbURI = config.db;
+mongoose.connect(dbURI,function (err) {
+    if(err)
+        console.log('mongoose error ',err);
+    else
+        console.log('connected to '+dbURI);
+});
+mongoose.connection.on('disconnected',function () {
+    console.log('Disconnected from Db at ',dbURI);
+});
+
+var gracefulShutdown = function (msg, cb) {
+    console.log('Disconnected from db due to '+msg);
+    cb();
+};
+
+process.once('SIGUSR2',function () {
+   gracefulShutdown('SIGUSR2',function () {
+        process.kill(process.pid,'SIGUSR2');
+   });
+});
+// process.on('SIGKILL',function () {
+//     gracefulShutdown('SIGKILL',function () {
+//         process.exit(0);
+//     });
+// });
+process.on('SIGINT',function () {
+    gracefulShutdown('SIGINT',function () {
+        process.exit(0);
+    });
+});
+process.on('SIGTERM',function () {
+    gracefulShutdown('SIGTERM',function () {
+        process.exit(0);
+    });
+});
